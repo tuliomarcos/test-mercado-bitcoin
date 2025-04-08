43fee36866635aea033e5514ada06bc82fcf2191
Tulio Marcos <tulio.silva.3@globalhitss.com.br>
2025-04-08 15:05:19 -0300

    fix: validate in the fourthStep

--- frontend/src/components/Form/steps/FourthStep.vue
+++ frontend/src/components/Form/steps/FourthStep.vue
@@ -7,26 +7,27 @@
     
     <Input v-model="formData.email" label="Endereço de e-mail" type="text" :error="errors.email" />
 
-    <template v-if="accountType === 'PF'">
+    <template v-if="formData.accountType === 'PF'">
       <Input v-model="formData.name" label="Nome" type="text" :error="errors.name" />
       <Input v-model="formData.document" label="CPF" type="text" :error="errors.document" />
       <Input v-model="formData.age" label="Data de nascimento" type="date" :error="errors.age" />
     </template>
     <template v-else>
-      <Input v-model="formData.name" label="Razão social" type="text" :error="errors.companyName" />
+      <Input v-model="formData.name" label="Razão social" type="text" :error="errors.nome" />
       <Input v-model="formData.document" label="CNPJ" type="text" :error="errors.document" />
-      <Input v-model="formData.age" label="Data de abertura" type="date" :error="errors.openDate" />
+      <Input v-model="formData.age" label="Data de abertura" type="date" :error="errors.age" />
     </template>
 
-    <Input v-model="formData.phone" label="Telefone" type="text" :error="errors.phone" />
+    <Input v-model="formData.phone" label="Telefone" type="text" :error="errors.phone" maxlength="15" />
     <Input v-model="formData.password" label="Senha" type="text" :error="errors.password" />
   </div>
 </template>
 
 <script setup>
-import { inject, reactive } from 'vue'
+import { inject, reactive, watch } from 'vue'
 import Input from '../Input.vue'
-import { isCpf, isEmail, isPassword, isPhone, isRequired, minLength } from '../../../utils/validator'
+import { isCNPJ, isCpf, isEmail, isPassword, isPhone, isRequired, minLength } from '../../../utils/validator'
+import { formatCnpj, formatCpf, formatPhone } from '../../../utils/formatter'
 
 const { currentStep, totalSteps } = inject('stepInfo')
 const formData = inject('formData')
@@ -44,12 +45,17 @@ function validate() {
   let valid = true
   Object.keys(errors).forEach(key => (errors[key] = ''))
 
-  errors.email = isRequired(formData.email, 'E-mail') || isEmail(formData.email)
-  errors.name = isRequired(formData.name, 'Nome') || minLength(formData.name, 3, 'Nome')
-  errors.document = isRequired(formData.document, 'CPF') || isCpf(formData.document)
-  errors.age = isRequired(formData.age, 'Data de nascimento')
+  if (formData.accountType === "PF") {
+    errors.name = isRequired(formData.name, 'Nome') || minLength(formData.name, 6, 'Nome')
+    errors.document = isRequired(formData.document, 'CPF') || isCpf(formData.document)
+    errors.age = isRequired(formData.age, 'Data de nascimento')
+  } else {
+    errors.name = isRequired(formData.name, 'Razão social') || minLength(formData.name, 6, 'Razão social')
+    errors.document = isRequired(formData.document, 'CNPJ') || isCNPJ(formData.document)
+    errors.age = isRequired(formData.age, 'Data de abertura')  
+  }
+
   errors.phone = isRequired(formData.phone, 'Telefone') || isPhone(formData.phone)
-  errors.password = isRequired(formData.password, 'Senha') || isPassword(formData.password)
 
   for (const key in errors) {
     if (errors[key]) valid = false
@@ -57,6 +63,20 @@ function validate() {
 
   return valid
 }
+
+watch(
+  () => formData.document,
+  (val) => {
+    formData.document = formData.accountType === 'PF' ? formatCpf(val) : formatCnpj(val);
+  }
+);
+
+watch(
+  () => formData.phone,
+  (val) => {
+    formData.phone = formatPhone(val);
+  }
+);
 
 defineExpose({ validate })
 </script>
